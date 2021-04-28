6f016dbaabf449e47a0f485fbc98dae7ecbf472e
AskhatMaksut <83279442+AskhatMaksut@users.noreply.github.com>
2021-04-28 23:21:31 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import subprocess
+import subprocess,telebot
 subprocess.call('soffice --headless --convert-to pdf:writer_pdf_Export --outdir /app *.docx',shell=True)
+bot = telebot.TeleBot('1679597751:AAHfoNzsYE4mcZpBqekcjTQP1euKgSGNoks')
+
+@bot.message_handler(content_types=['text'])
+def echo_all(message):
+   bot.send_document(message.chat.id, open(r'app/hello.pdf, 'rb'))
+
+
+bot.polling(True)
