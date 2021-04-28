912a327c1b5398b9ffb4926a7525724f8a3601b8
AskhatMaksut <83279442+AskhatMaksut@users.noreply.github.com>
2021-04-28 21:56:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 import subprocess
-subprocess.call('soffice --headless --convert-to pdf:writer_pdf_Export --outdir /app *.docx')
+subprocess.call('soffice --headless --convert-to pdf:writer_pdf_Export --outdir /app *.docx',shell=True)
