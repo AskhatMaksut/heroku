7a9bc467ca751722c39a74d2a4d2858c24e6f20c
AskhatMaksut <83279442+AskhatMaksut@users.noreply.github.com>
2021-04-30 16:44:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('1679597751:AAHfoNzsYE4mcZpBqekcjTQP1euKgSGNoks')
 @bot.message_handler(content_types=['text'])
 def echo_all(message):
    if message.text == 'Hello':
-       uis_pdf = open('app/hello.pdf', 'rb')
+       uis_pdf = open('app/hello.docx', 'rb')
        bot.send_document(message.chat.id, uis_pdf)
        uis_pdf.close()
 
