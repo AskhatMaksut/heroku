2d4b1c3f71358b9e99cc37d9e4ea7b460f0ae98e
AskhatMaksut <83279442+AskhatMaksut@users.noreply.github.com>
2021-05-01 20:49:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess,telebot
 bot = telebot.TeleBot('1679597751:AAHfoNzsYE4mcZpBqekcjTQP1euKgSGNoks')
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['document'])
 def echo_all(message):
     file_name = message.document.file_name
     file_id_info = bot.get_file(message.document.file_id)
