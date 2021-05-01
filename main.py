869319f80cb06d03f51053d110976d12302e6875
AskhatMaksut <83279442+AskhatMaksut@users.noreply.github.com>
2021-05-01 19:49:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import subprocess,telebot
-subprocess.call('soffice --headless --convert-to pdf:writer_pdf_Export --outdir /app *.docx',shell=True)
 bot = telebot.TeleBot('1679597751:AAHfoNzsYE4mcZpBqekcjTQP1euKgSGNoks')
 
 @bot.message_handler(content_types=['text'])
 def echo_all(message):
-   if message.text == 'Hello':
-       uis_pdf = open('/app/hello.pdf', 'rb')
-       bot.send_document(message.chat.id, uis_pdf)
-       uis_pdf.close()
+    file_name = message.document.file_name
+    file_id_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_id_info.file_path)
+    src = file_name
+    with open(r'app\{}'.format(src), 'wb') as new_file:
+        new_file.write(downloaded_file)
+    subprocess.call('soffice --headless --convert-to pdf:writer_pdf_Export --outdir /app/{}'.format(src),shell=True)
+    uis_pdf = open('/app/{}'.format(src), 'rb')
+    bot.send_document(message.chat.id, uis_pdf)
+    uis_pdf.close()
+    import os,time
+    time.sleep(10)
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), str(src))
+    os.remove(path)
+    
 
 
 bot.polling(True)
