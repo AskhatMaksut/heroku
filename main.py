312a61cfc966f26bded8a9ab59cd3773e8557aff
AskhatMaksut <83279442+AskhatMaksut@users.noreply.github.com>
2021-05-03 22:38:39 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def echo_all(message):
         new_file.write(downloaded_file)
         print('Successfly created new file')
     time.sleep(10)
-    subprocess.call('soffice --headless --convert-to pdf {}'.format(src))
+    subprocess.call('soffice --headless --convert-to pdf /app/{}'.format(src))
    
    
     
